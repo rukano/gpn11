9dc74a68ae5a58cbb6dc14f04e8ce3a07e7525a9
hb <hb@uiae.de>
2011-06-24 00:21:01 +0200

    hm

--- main.lua
+++ main.lua
@@ -28,8 +28,18 @@ function love.load()
 
    player = {}
    player.image = love.graphics.newImage("img/player.png")
-   player.x = 50
-   player.y = 50
+   player.pos = Vector.new(50, 50)
+   player.dpos = Vector.new(0, 0)
+   player.rotation = 0
+   player.drot = 0
+   player.update = function(dt)
+                      player.drot = player.drot - (1 * dt)
+                      player.dpos = player.dpos - (1 * dt)
+                   end
+   player.push = function(dt, direction)
+                    player.drot = math.max(player.drot + (dt * 1 * direction), 0.1)
+                 end
+   
 
    -- physics world
    world = world or love.physics.newWorld(-width, -height, width, height)
@@ -56,20 +66,26 @@ end
 function love.update(dt)
    world:update(dt)
    if love.keyboard.isDown("left") then
-
+      player.push(dt, -1)
+      -- drehen links
    elseif love.keyboard.isDown("right") then
-
+      -- drehen rechts
+      player.push(dt, 1)
    elseif love.keyboard.isDown("up") then
-
+      -- entmagnetisieren
+      player.magnetic = false
    elseif love.keyboard.isDown("down") then
-
+      -- magnetisieren
+      player.magnetic = true
    end
+
+   player.update(dt)
 end
 
 --------------------------------------------------------------------------------
 -- DRAW
 function love.draw()
-   love.graphics.draw(player.image, player.x, player.y)
+   love.graphics.draw(player.image, player.pos:unpack(), player.rot)
 end
 
 
