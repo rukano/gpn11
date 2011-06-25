009513c1a38146ef5690f730e22b12796c1868d1
Juan A. Romero <rukano@gmail.com>
2011-06-25 03:29:30 +0200

    added credits gamestate

---
+++ credits.lua
@@ -0,0 +1,28 @@
+credits = Gamestate.new()
+
+function credits:init ()
+
+end
+
+function credits:enter(previous)
+end
+
+function credits:leave()
+
+end
+
+function credits:update(dt)
+
+end
+
+function credits:draw()
+
+end
+
+function credits:keypressed(key)
+
+end
+
+function credits:mousepressed(x, y, btn)
+
+end
