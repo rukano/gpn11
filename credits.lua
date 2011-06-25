aee1ad2754e60e2d00f777e4ce84a06c54484044
Juan A. Romero <rukano@gmail.com>
2011-06-25 21:59:04 +0200

    credits implemented

--- credits.lua
+++ credits.lua
@@ -1,28 +1,30 @@
 credits = Gamestate.new()
 
-function credits:init ()
-
+function credits:enter (prev)
+   love.graphics.setFont(font)
+   credits_music = love.audio.play("music/menu.ogg", "stream", true)
 end
 
-function credits:enter(previous)
+function credits:leave ()
+   love.audio.stop(credits_music)
 end
 
-function credits:leave()
-
+function credits:update (dt)
+   updateStars(dt)
 end
 
-function credits:update(dt)
+function credits:draw ()
+   love.graphics.setBackgroundColor(0, 0, 0, 255)
 
+   drawStars()
+
+   love.graphics.setColor(200, 200, 255, 255)
+   love.graphics.print("rukano", 100, 100)
+   love.graphics.print("hb", 100, 200)
 end
 
-function credits:draw()
-
-end
-
-function credits:keypressed(key)
-
-end
-
-function credits:mousepressed(x, y, btn)
-
+function credits:keypressed (key, code)
+   if key == "escape" then
+      Gamestate.switch(menu)
+   end
 end
